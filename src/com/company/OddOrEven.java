0ac2c44908ab78f0b774d3448f1cff3069574458
ndantelugo <ndantelugo@gmail.com>
2021-11-18 23:13:31 -0500

    initial commit

--- src/com/company/OddOrEven.java
+++ src/com/company/OddOrEven.java
@@ -1,4 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class OddOrEven {
+
+    public static void main(String[] args) {
+        //accept input and output if odd or even
+        Scanner input = new Scanner(System.in);
+        int num=input.nextInt();
+        String test;
+        int nummod;
+        nummod = num%2;
+        if (nummod==0){
+            test = "even";
+        }
+        else{
+            test="odd";
+        }
+        System.out.println(num+" is "+test);
+
+    }
 }
